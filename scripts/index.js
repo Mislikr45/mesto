34493d2b48b05c676203200ef599094cf365ba99
Sergey Pozdnyakov <110182897+Mislikr45@users.noreply.github.com>
2022-12-06 23:25:37 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,36 @@
 const popupSave=document.querySelector('.button_function_save')
 let popup = document.querySelector('.popup');
 let popupContent = popup.querySelector('.popup__content');
-let nameInput = popupContent.querySelector('.popup__name');
-let popUpAbout =  popupContent.querySelector('.popup__about');
+let nameInput = document.querySelector('.popup__input_type_name');
+let aboutInput = document.querySelector('.popup__input_type_about');
 let closeButton = popup.querySelector('.button_function_close');
 let editButton = document.querySelector('.button_function_edite');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 let like=document.querySelector('.like');
-
+console.log({nameInput,aboutInput})
 
 function closePopup () {
-  popup.classList.remove('popup_open');
+  popup.classList.remove('popup_opened');
 };
 
 function openPopup () {
-  popup.classList.add('popup_open');
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
-  popUpAbout.value = profileAbout.textContent;
+  aboutInput.value = profileAbout.textContent;
 };
 
 function handleFormSubmit (evt) {
     evt.preventDefault(evt),
     profileName.textContent = nameInput.value,
-    profileAbout.textContent = popUpAbout.value,
+    profileAbout.textContent = aboutInput.value,
     closePopup();
 }
 
 like.addEventListener('click', function(likeElement) {
-    like.classList.toggle('like_active_selected')}
-    )
+    like.classList.toggle('like_selected')})
 
-popupSave.addEventListener('click', handleFormSubmit);
+popup.addEventListener('submit', handleFormSubmit);
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
  
