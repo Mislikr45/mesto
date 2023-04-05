9bc8949653b8fb36a387597b1cfe656b817a1463
Sergey Pozdnyakov <110182897+Mislikr45@users.noreply.github.com>
2023-04-05 23:49:19 +0500

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,7 +1,18 @@
-import { closeByEsc } from "./index.js";
 function openPopup(popup) {
-  popup.classList.add("popup_opened");
-  document.addEventListener("keydown", closeByEsc);
-}
-
-export default openPopup;
+    popup.classList.add("popup_opened");
+    document.addEventListener("keydown", closeByEsc);
+  }
+  
+  function closeByEsc(evt) {
+    if (evt.key === "Escape") {
+      const openedPopup = document.querySelector(".popup_opened");
+      closePopup(openedPopup);
+    }
+  }
+  
+  function closePopup(popup) {
+    popup.classList.remove("popup_opened");
+    document.removeEventListener("keydown", closeByEsc);
+  }
+  export { openPopup, closeByEsc, closePopup };
+  
